a03b8d52103ea99d385ca200dac7febf76d45a86
alastaira <alastair@playful.technology>
2022-03-20 16:22:42 +0000

    Update VS1053.ino

--- VS1053/VS1053.ino
+++ VS1053/VS1053.ino
@@ -21,14 +21,14 @@
 #include <Adafruit_VS1053.h>
 
 // CONSTANTS
-// When grounded, will cause sound effect to play. This is wired through the RC35 magnet sensors and the plunger switch
-const byte inputPin = 3;
+const byte recPin = 3;
+const byte playbackPin = 4;
 
 // GLOBALS
 Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
 bool isPlaying = false;
 uint8_t isRecording = false;
-File recording;  // the file we will save our recording to
+File saveFile;  // the file we will save our recording to
 #define RECBUFFSIZE 128  // 64 or 128 bytes.
 uint8_t recording_buffer[RECBUFFSIZE];
 
@@ -36,23 +36,25 @@ void setup() {
   Serial.begin(115200);
   Serial.println(__FILE__ __DATE__);
 
+  Serial.print(F("Initialising VS1053..."));
   if(!musicPlayer.begin()) { // Initialise the VS1053 MP3 player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     Serial.println(F("FAILED. Check you have the correct pins defined"));
      while(1);
   }
-  Serial.println(F("VS1053 found"));
+  Serial.println("OK");
 
   delay(500);
-  
+
+  Serial.print(F("Initialising SD Card..."));
   if(!SD.begin(CARDCS)) {
-    Serial.println(F("Could not initialise SD Card"));
+    Serial.println(F("FAILED"));
     while (1);
   }
-  Serial.println(F("SD Card initialised"));
+  Serial.println(F("OK"));
 
   delay(500);
 
-
+  Serial.print(F("Loading VS1053 Firmware..."));
   // Load VS1053 firmware from SD card.
   // Different profiles can be downloaded from VS1053 Ogg Vorbis Encoder Application
   // http://www.vlsi.fi/en/support/software/vs10xxapplications.html
@@ -60,10 +62,10 @@ void setup() {
   // We'll use mono 44.1KHz, high quality
   // v16k1q05 is Wideband voice, mono, mid-quality
   if (! musicPlayer.prepareRecordOgg("v16k1q05.img")) {
-     Serial.println("Couldn't load profile!");
+     Serial.println("FAILED");
      while (1);    
   }
-
+  Serial.println(F("OK"));
 
 /*
   // List files
@@ -73,104 +75,61 @@ void setup() {
   musicPlayer.setVolume(5,5);
 */
 
-
-
-
   // If DREQ is on an interrupt pin (on uno, #2 or #3) we can play audio in background
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
 
  // musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working  
 
-  
-  // Play one file, don't return until complete
-  // Serial.println(F("Playing track 001"));
-  // musicPlayer.playFullFile("CHINK.MP3");
-
-//delay(5000);
-  
-  // Play another file in the background, REQUIRES interrupts!
-  // Serial.println(F("Playing track 002"));
-  // musicPlayer.startPlayingFile("/track002.mp3");
-
   // Declare pinmodes
-  pinMode(inputPin, INPUT_PULLUP);
+  pinMode(recPin, INPUT_PULLUP);
 
   musicPlayer.sineTest(0x44, 100);
   musicPlayer.sineTest(0x50, 100);
 }
 
 void loop() {
-/*
-    if(!digitalRead(inputPin)){
-      musicPlayer.playFullFile("02WATC~1.MP3");
-    }
-*/
-
-  if (!isRecording && !digitalRead(inputPin)) {
-    Serial.println("Begin recording");
-    isRecording = true;
-    
-    // Check if the file exists already
-    char filename[15];
-    strcpy(filename, "RECORD00.OGG");
-    for (uint8_t i = 0; i < 100; i++) {
-      filename[6] = '0' + i/10;
-      filename[7] = '0' + i%10;
-      // create if does not exist, do not open existing, write, sync after write
-      if (! SD.exists(filename)) {
-        break;
-      }
-    }
-    Serial.print("Recording to "); Serial.println(filename);
-    recording = SD.open(filename, FILE_WRITE);
-    if (! recording) {
-       Serial.println("Couldn't open file to record!");
-       while (1);
-    }
-    musicPlayer.startRecordOgg(true); // use microphone (for linein, pass in 'false')
-  }
-  if (isRecording)
-    saveRecordedData(isRecording);
-  if (isRecording && digitalRead(inputPin)) {
-    Serial.println("End recording");
-    musicPlayer.stopRecordOgg();
-    isRecording = false;
-    // flush all the data!
-    saveRecordedData(isRecording);
-    // close it up
-    recording.close();
-    delay(1000);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   
-  //  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working  
-
- // delay(10000);
-
-  /*
-  if(!digitalRead(inputPin)){
-    Serial.println(F("Explode"));
-    digitalWrite(relayPin, LOW);
-    musicPlayer.playFullFile("/explode.mp3");
-    delay(3000);
-    digitalWrite(relayPin, HIGH);  
+  if(!isRecording) {
+    if(!digitalRead(recPin)) {
+      Serial.println(F("Begin recording"));
+      isRecording = true;
+      
+      // Check if the file exists already
+      char filename[15];
+      strcpy(filename, "RECORD00.OGG");
+      for (uint8_t i = 0; i < 100; i++) {
+        filename[6] = '0' + i/10;
+        filename[7] = '0' + i%10;
+        // create if does not exist, do not open existing, write, sync after write
+        if (! SD.exists(filename)) {
+          break;
+        }
+      }
+      Serial.print(F("Recording to ")); Serial.println(filename);
+      saveFile = SD.open(filename, FILE_WRITE);
+      if (!saveFile) {
+         Serial.println(F("Couldn't open file to record!"));
+         while (1);
+      }
+      musicPlayer.startRecordOgg(true); // use microphone (for linein, pass in 'false')
+    }
   }
-  */
+  if(isRecording) {
+    // Continuously save audio stream to SD card
+    saveRecordedData(isRecording);
+    
+    if(digitalRead(recPin)) {
+      Serial.println("End recording");
+      musicPlayer.stopRecordOgg();
+      isRecording = false;
+      // flush all the data!
+      saveRecordedData(isRecording);
+      // close it up
+      saveFile.close();
+      delay(1000);
+    }
+  }
+  
 }
 
 /// File listing helper
@@ -207,7 +166,7 @@ uint16_t saveRecordedData(boolean isrecord) {
   
   // try to process 256 words (512 bytes) at a time, for best speed
   while (wordswaiting > 256) {
-    //Serial.print("Waiting: "); Serial.println(wordswaiting);
+    Serial.print("Waiting: "); Serial.println(wordswaiting);
     // for example 128 bytes x 4 loops = 512 bytes
     for (int x=0; x < 512/RECBUFFSIZE; x++) {
       // fill the buffer!
@@ -217,13 +176,13 @@ uint16_t saveRecordedData(boolean isrecord) {
         recording_buffer[addr] = t >> 8; 
         recording_buffer[addr+1] = t;
       }
-      if (! recording.write(recording_buffer, RECBUFFSIZE)) {
+      if (! saveFile.write(recording_buffer, RECBUFFSIZE)) {
             Serial.print("Couldn't write "); Serial.println(RECBUFFSIZE); 
             while (1);
       }
     }
     // flush 512 bytes at a time
-    recording.flush();
+    saveFile.flush();
     written += 256;
     wordswaiting -= 256;
   }
@@ -239,26 +198,26 @@ uint16_t saveRecordedData(boolean isrecord) {
       recording_buffer[addr] = t >> 8; 
       recording_buffer[addr+1] = t;
       if (addr > RECBUFFSIZE) {
-          if (! recording.write(recording_buffer, RECBUFFSIZE)) {
+          if (! saveFile.write(recording_buffer, RECBUFFSIZE)) {
                 Serial.println("Couldn't write!");
                 while (1);
           }
-          recording.flush();
+          saveFile.flush();
           addr = 0;
       }
     }
     if (addr != 0) {
-      if (!recording.write(recording_buffer, addr)) {
+      if (!saveFile.write(recording_buffer, addr)) {
         Serial.println("Couldn't write!"); while (1);
       }
       written += addr;
     }
     musicPlayer.sciRead(VS1053_SCI_AICTRL3);
     if (! (musicPlayer.sciRead(VS1053_SCI_AICTRL3) & (1 << 2))) {
-       recording.write(musicPlayer.recordedReadWord() & 0xFF);
+       saveFile.write(musicPlayer.recordedReadWord() & 0xFF);
        written++;
     }
-    recording.flush();
+    saveFile.flush();
   }
 
   return written;
